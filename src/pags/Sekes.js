d421bf0bd2835b0efca70707d001ba8e4479d7fa
yeonji123 <duathshdwkd1@naver.com>
2022-10-25 22:56:13 +0900

    1025 연지

--- src/pags/Sekes.js
+++ src/pags/Sekes.js
@@ -6,6 +6,7 @@ import { useParams } from "react-router-dom";
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import CreateMeetModal from '../Components/CreateMeetModal'
+import noData from '../img/NoData.png';
 
 const goClient = () => {
   const l = window.location.href;
@@ -22,33 +23,37 @@ const Sekes = () => {
   const [starthome, setModal] = useState(false);
   const [roomName, setRoomName] = useState();
 
-  const testonclick = idx => {
+  
+  const ingmeetclick = idx => {
     console.log(idx);
-    window.location.href = '/Sekes/' + idx;
-
-  }
-  const ingmeetclick = idx =>{
-    console.log(idx);
-    window.location.href = '/Client/'+idx;
+    const location = window.location.href;
+    var room = parseInt(location.split("/")[4]); //그룹 이름
+    //클라이언트 그 콘텐츠 
+    window.location.href = '/Client/' + room + '/' + idx; //Client/그룹num/회의번호
   }
 
   useEffect(() => {
     const location = window.location.href;
     var room = parseInt(location.split("/")[4]);
     
-    //창훈이형 그룹 내 회의 목록
     (async () => {
       try {
-        const res = await axios.post('http://192.168.2.65:5000/readMeeting', {
+        const res = await axios.post('http://192.168.2.65:5000/readMeeting', { //창훈이형 그룹 내 회의 목록
           roomNum: room
         });
-        // console.log(res.data);
-        setData(res.data);
-        // console.log(res.data[0].roomName);
-        setRoomName(res.data[0].roomName);
+
+
+        if (res.data[0].meetmNum!=0){
+          console.log("데이터가 있음");
+          setData(res.data);
+          console.log(res.data);
+          setRoomName(res.data[0].roomName);
+        }
+
 
       } catch (error) {
         console.log(error)
+        setIsHave(false);
       }
     })();
 
@@ -62,7 +67,7 @@ const Sekes = () => {
         });
         console.log(ing.data);
         setMeetData(ing.data);
-
+        setRoomName(ing.data[0].roomName);
       } catch (error) {
         console.log(error)
       }
@@ -99,23 +104,36 @@ const Sekes = () => {
 
           <tbody>
             {
-
+              meetData && meetData.map((e, idx) =>
+                <tr id="test" class="blinking" onClick={() => ingmeetclick(e.meetingRoomNum)}>
+                  <th> ing...</th>
+                  <th>{e.meetingRoomTitle}</th> {/* 회의 이름 */}
+                  <th></th> {/* 회의 종료 날짜 */}
+                  <th>{e.meetingRoomHost}</th> {/* 회의 만든 사람 */}
+                </tr>
+              )
               
             }
             
             {
               data && data.map((e, idx) =>
-                <tr  id="test" class="blinking" onClick={() => testonclick(e.roomNum)}>
+                <tr  id="test" class="blinking" onClick={() => ingmeetclick(e.roomNum)}>
                   {/* id="test" class="blinking" 위에 깜박 거리는 css*/ }
                   <th> {idx + 1}</th>
                   <th >{e.meetTitle}</th> {/* 회의 이름 */}
                   <th>{e.meetDate}</th> {/* 회의 종료 날짜 */}
-                  <th>{e.meethost}</th> {/* 회의 만든 사람 */}
                 </tr>
               )
             }
           </tbody>
         </Table>
+        <div id="noData"> 
+            {
+              ishave ? <div></div> : <div><img src={noData}/></div>
+
+            }
+          
+        </div>
       </Box>
     </Box>
   )
