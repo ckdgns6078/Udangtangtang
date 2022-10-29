8d5049d7e1a0ba3fd83e59a1da53a65f7e0e90db
yeonji123 <duathshdwkd1@naver.com>
2022-10-29 22:31:19 +0900

    유효성 검사, 데이터 없음 삼항연산자 추가1029
    유효성 검사 HomeLest, Modal, CreateMeetModal
    재문이형 코드
    삼항연산자 추가

--- src/Components/CreateMeetModal.js
+++ src/Components/CreateMeetModal.js
@@ -7,28 +7,47 @@ import Form from 'react-bootstrap/Form';
 import axios from "axios";
 
 const CreateMeetModal = ({ show, onHide }) => {
-  const [meetingRoomTitle, setmeetRoomTitle] = useState("");
- 
-  
+  const [meetingRoomTitle, setMeetRoomTitle] = useState("");
+
+  const [testMeetRoom, setTestMeetRoom] = useState(true);
+
+  const checkInput = /^[가-힣a-zA-Z0-9]{2,15}$/; //유효성 검사
+
+  const MeetingRoom = (e) => {
+    // setRoomName(e.target.value); //이벤트가 발생되면 저장 -- 유효성 검사 후 저장으로 변환해야함
+    const check = e.target.value;
+    console.log(check);
+    console.log("유효성 검사 : ", checkInput.test(e.target.value));
+    if (checkInput.test(e.target.value)) { //true이면, 즉 유효성검사를 통과하면
+      setMeetRoomTitle(e.target.value);
+      setTestMeetRoom(true); //true이면 유효성 경고창 안보이게
+    } else {
+      setTestMeetRoom(false);
+    }
+  }
+
 
   const handleSubmit = async () => {
     const location = window.location.href;
     var room = parseInt(location.split("/")[4]);
-    // axios로 서버에 보낸다
-    try {
-      const res = await axios.post('http://192.168.2.82:5000/createMeetingRoom', {
-        roomNum : room,
-        meetingRoomTitle: meetingRoomTitle,
-        meetingRoomHost: sessionStorage.getItem("id")
-      })
-      console.log(res.data.meetingRoomNum);
-      window.location.href="http://localhost:3000/Client/"+room+"/"+res.data[0].meetingRoomNum;
+    if (testMeetRoom) { //true 이면
+      // axios로 서버에 보낸다
+      try {
+        const res = await axios.post('http://192.168.2.82:5000/createMeetingRoom', {
+          roomNum: room,
+          meetingRoomTitle: meetingRoomTitle,
+          meetingRoomHost: sessionStorage.getItem("id")
+        })
+        console.log(res.data.meetingRoomNum);
+        window.location.href = "http://localhost:3000/Client/" + room + "/" + res.data[0].meetingRoomNum;
+      }
+      catch (e) {
+        console.error(e);
+      }
+      setTestMeetRoom(true);
     }
-    catch (e) {
-      console.error(e);
-    }
-    
   }
+
 
 
   return (
@@ -50,9 +69,11 @@ const CreateMeetModal = ({ show, onHide }) => {
         <Form>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>회의 제목 </Form.Label>
-            <Form.Control type="text" placeholder="방제목 입력하세요" onChange={(e) => setmeetRoomTitle(e.target.value)} />
+            <Form.Control type="text" placeholder="방제목 입력하세요" onChange={MeetingRoom} />
+            {
+              testMeetRoom ? <div></div> : <div style={{ color: "red" }} > 한글, 영문, 숫자로 2~15자 이내로 작성해주세요 </div>
+            }
           </Form.Group>
-
         </Form>
       </Modal.Body>
       <Modal.Footer>
